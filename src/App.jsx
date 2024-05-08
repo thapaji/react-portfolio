62553db803325772049b24c3cb599826440c2199
Sujan <mail.thapasujan@gmail.com>
2024-05-08 12:23:00 +1000

    Saving changes before restarting coding

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
+import { NavBar } from "./components/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-    </>
-  )
+    <div className="App">
+      <NavBar />
+    </div>
+  );
 }
 
-export default App
+export default App;
