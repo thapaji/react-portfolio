f35409b7aa1330a1e3c6ce5c5d9b70caacfe621f
Sujan <mail.thapasujan@gmail.com>
2024-05-08 15:15:33 +1000

    Project and Skill component implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from "react";
 import { NavBar } from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import "./App.css";
 
 function App() {
-  
-
   return (
     <div className="App">
       <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
     </div>
   );
 }
